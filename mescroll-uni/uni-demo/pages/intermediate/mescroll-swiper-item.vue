4f961669e2d53688ae48c88c7315dc50d6834357
mescroll <mescroll@qq.com>
2019-06-28 15:58:12 +0800

    新增swiper案例

---
+++ mescroll-uni/uni-demo/pages/intermediate/mescroll-swiper-item.vue
@@ -0,0 +1,135 @@
+<template>
+	<!-- top是指mescroll的padding-top的数值,单位upx. 目的是使下拉布局往下偏移,不然会被悬浮菜单遮住 -->
+	<mescroll-uni top="60" :down="downOption" @down="downCallback" :up="upOption" @up="upCallback" @init="mescrollInit" @emptyclick="emptyClick">
+		<!-- 数据列表 -->
+		<pd-list :list="pdList"></pd-list>
+	</mescroll-uni>
+</template>
+
+<script>
+	import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue";
+	import PdList from "@/components/other/pd-list.vue";
+	import mockData from "@/common/pdlist.js"; // 模拟数据
+	
+	export default {
+		components: {
+			MescrollUni,
+			PdList
+		},
+		data() {
+			return {
+				mescroll: null, //mescroll实例对象
+				downOption:{
+					auto:false, // 不自动加载
+				},
+				upOption:{
+					auto:false, // 不自动加载
+					// page: {
+					// 	num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
+					// 	size: 10 // 每页数据的数量
+					// },
+					noMoreSize: 4, //如果列表已无数据,可设置列表的总数量要大于半页才显示无更多数据;避免列表数据过少(比如只有一条数据),显示无更多数据会不好看; 默认5
+					empty:{
+						tip: '~ 空空如也 ~', // 提示
+						btnText: '去看看'
+					}
+				},
+				pdList: [], //列表数据
+				isInit: false, // 列表是否已经初始化
+				scrollY: 0
+			}
+		},
+		props:{
+			i: Number, // 每个tab页的专属下标
+			index: { // 当前tab的下标
+				type: Number,
+				default(){
+					return 0
+				}
+			}
+		},
+		watch:{
+			// 监听下标的变化
+			index(val){
+				if (this.i === val && !this.isInit) {
+					this.isInit = true; // 标记为true
+					this.mescroll.triggerDownScroll();
+				}
+			}
+		},
+		mounted() {
+			// 第一个tab,自动加载数据
+			if(this.i === 0){
+				this.isInit = true; // 标记为true
+				this.mescroll.triggerDownScroll();
+			}
+		},
+		methods: {
+			// mescroll组件初始化的回调,可获取到mescroll对象
+			mescrollInit(mescroll) {
+				this.mescroll = mescroll;
+			},
+			/*下拉刷新的回调 */
+			downCallback(mescroll) {
+				// 这里加载你想下拉刷新的数据, 比如刷新轮播数据
+				// loadSwiper();
+				// 下拉刷新的回调,默认重置上拉加载列表为第一页 (自动执行 mescroll.num=1, 再触发upCallback方法 )
+				mescroll.resetUpScroll()
+			},
+			/*上拉加载的回调: mescroll携带page的参数, 其中num:当前页 从1开始, size:每页数据条数,默认10 */
+			upCallback(mescroll) {
+				//联网加载数据
+				this.getListDataFromNet(mescroll.num, mescroll.size, (curPageData)=>{
+					//联网成功的回调,隐藏下拉刷新和上拉加载的状态;
+					console.log("i="+this.i+", mescroll.num=" + mescroll.num + ", mescroll.size=" + mescroll.size + ", curPageData.length=" + curPageData.length);
+					mescroll.endSuccess(curPageData.length);
+					//设置列表数据
+					if(mescroll.num == 1) this.pdList = []; //如果是第一页需手动制空列表
+					this.pdList=this.pdList.concat(curPageData); //追加新数据
+				}, () => {
+					//联网失败的回调,隐藏下拉刷新的状态
+					mescroll.endErr();
+				})
+			},
+			//点击空布局按钮的回调
+			emptyClick(){
+				uni.showToast({
+					title:'点击了按钮,具体逻辑自行实现'
+				})
+			},
+			
+			/*联网加载列表数据
+			在您的实际项目中,请参考官方写法: http://www.mescroll.com/uni.html#tagUpCallback
+			请忽略getListDataFromNet的逻辑,这里仅仅是在本地模拟分页数据,本地演示用
+			实际项目以您服务器接口返回的数据为准,无需本地处理分页.
+			* */
+			getListDataFromNet(pageNum,pageSize,successCallback,errorCallback) {
+				//延时一秒,模拟联网
+				setTimeout(()=> {
+					try{
+						let listData = []
+						if (this.i === 0) {
+							for (let i = (pageNum - 1) * pageSize; i < pageNum * pageSize; i++) {
+								if (i === mockData.length) break
+								listData.push(mockData[i])
+							}
+						} else{
+							let wordArr = ['', '奶粉', '面膜', '图书']
+							let word = wordArr[this.i]
+							for (let i = 0; i < mockData.length; i++) {
+								if (mockData[i].pdName.indexOf(word) !== -1) {
+									listData.push(mockData[i])
+								}
+							}
+						}
+						// 回调
+						successCallback && successCallback(listData);
+					} catch (e) {
+						//联网失败的回调
+						errorCallback && errorCallback();
+					}
+				},1000)
+			}
+		}
+	}
+</script>
---
+++ mescroll-uni/uni-demo/pages/intermediate/mescroll-swiper.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="page-warp">
+		<view class="top-warp">
+			<view v-for="(tab, i) in tabs" :key="i" :class="{'active':curIndex===i}" @click="changeTab(i)">{{tab}}</view>
+		</view>
+		
+		<swiper class="swiper" :current="curIndex" @change="swiperChange">
+			<!--全部 -->
+			<swiper-item>
+				<mescroll-item :i="0" :index="curIndex"></mescroll-item>
+			</swiper-item>
+			
+			<!-- 奶粉 -->
+			<swiper-item>
+				<mescroll-item :i="1" :index="curIndex"></mescroll-item>
+			</swiper-item>
+			
+			<!-- 面膜 -->
+			<swiper-item>
+				<mescroll-item :i="2" :index="curIndex"></mescroll-item>
+			</swiper-item>
+			
+			<!-- 图书 -->
+			<swiper-item>
+				<mescroll-item :i="3" :index="curIndex"></mescroll-item>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	import MescrollItem from "./mescroll-swiper-item.vue";
+	
+	export default {
+		components: {
+			MescrollItem
+		},
+		data() {
+			return {
+				tabs: ['全部', '奶粉', '面膜', '图书'],
+				curIndex: 0 // 当前tab的下标
+			}
+		},
+		methods: {
+			// 轮播菜单
+			swiperChange(e){
+				this.changeTab(e.detail.current)
+			},
+			// 切换菜单
+			changeTab(i){
+				this.curIndex = i
+			}
+		}
+	}
+</script>
+
+<style>
+	/* 需固定高度 */
+	page,
+	.page-warp,
+	.swiper{
+		height: 100%;
+	}
+	
+	.top-warp{
+		z-index: 9990;
+		position: fixed;
+		top: --window-top; /* css变量 */
+		left: 0;
+		width: 100%;
+		height: 60upx;
+		background-color: white;
+		text-align: center;
+		border-bottom: 1upx solid #ddd;
+	}
+	.top-warp view{
+		display: inline-block;
+		width: 22%;
+		line-height: 60upx;
+		font-size: 28upx;
+	}
+	.top-warp .active{
+		border-bottom: 2upx solid #FF6990;
+		color: #FF6990;
+	}
+</style>
